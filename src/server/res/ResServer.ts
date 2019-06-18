64bb9e5eee11c5b36d1cdef3dcf1606bae345136
lintao_alex <lintao_alex@163.com>
2019-06-18 19:40:55 +0800

    resource is ready

---
+++ src/server/res/ResServer.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by lintao_alex on 2019/6/18
+ */
+
+namespace Dream.frame {
+    import PromiseTaskReporter = RES.PromiseTaskReporter;
+
+    export class ResServer extends BaseServer {
+        $loadConfig(url: string, resourceRoot: string) {
+            return RES.loadConfig(url, resourceRoot);
+        }
+
+        loadGroup(name: string, priority?: number, reporter?: PromiseTaskReporter) {
+            return RES.loadGroup(name, priority, reporter);
+        }
+
+        getRes(key: string) {
+            return RES.getRes(key);
+        }
+    }
+}
